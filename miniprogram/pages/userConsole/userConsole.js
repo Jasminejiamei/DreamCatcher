d76d82feb9f314ee2b640c59645f5f3258f02c14
Jasminejiamei <1121663274@qq.com>
2019-10-26 17:26:59 +0800

    环境搭建
    环境基本搭建好了，克隆时要详细看markdown文档噢。

--- miniprogram/pages/userConsole/userConsole.js
+++ miniprogram/pages/userConsole/userConsole.js
@@ -1,10 +1,13 @@
 // pages/userConsole/userConsole.js
 Page({
-
+  //要渲染的数据变量定义，在请求数据的时候去赋值
   data: {
     openid: ''
   },
 
+  /**
+   * 向后台请求数据，setData，详情看开发文档的api，通过这样的方式做到数据动态渲染
+   */
   onLoad: function (options) {
     this.setData({
       openid: getApp().globalData.openid
