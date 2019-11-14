a40cc57eb983ccbcb12faddfe9deea44b0be6bb5
caiyouling <13427857046@163.com>
2019-11-14 23:38:44 +0800

    添加目标初步完成

--- miniprogram/pages/addTarget/addTarget.js
+++ miniprogram/pages/addTarget/addTarget.js
@@ -1,6 +1,11 @@
 // miniprogram/pages/addTarget/addTarget.js
-// 导入util
-// var util = require('../../utils/util.js');
+// 用于导入消息通知notify
+import Notify from '../../miniprogram_npm/vant-weapp/notify/notify';
+const db = wx.cloud.database();
+//判断是否传入数据库
+var canSend;
+//获取标签id
+let id ;
 Page({
   /**
    * 页面的初始数据
@@ -8,11 +13,12 @@ Page({
   data: {
    modalHidden:true,
    check:false,
-   type:"default",
+   idUpload:false,
+   isAnonymous:false,
     buttons: [
       { id: 1, name: '学习',src:'edit' },
       { id: 2, name: '旅游',src:'logistics'}, 
-     { id: 3, name: '技能',src:'award-o'},
+      { id: 3, name: '技能',src:'award-o'},
       { id: 4, name: '其他',src:'good-job-o'}],
   },
 
@@ -48,8 +54,7 @@ Page({
 // 设置选中标签
   radioButtonTap: function (e) {
     // 选中的id
-    let id = e.currentTarget.dataset.id
-    console.log(id)
+     id = e.currentTarget.dataset.id
     for (let i = 0; i < this.data.buttons.length; i++) {
       if (this.data.buttons[i].id == id) {
         //当前点击的位置为true即选中
@@ -73,49 +78,62 @@ Page({
     });
     // 如果选中了则弹出询问是否需要匿名框
     if(this.data.check==false){
-      console.log("没有选中发布");
+      console.log("不要发布广场");
     }else{
-      console.log("当按下保存键才进行检查");
+      this.setData({
+        modalHidden:!this.data.modalHidden,
+        isUpload:!this.data.isUpload
+      });
     }
   },
   // 保存点击事件需要检查输入的内容
-  save:function(){
+  addTarget:function(e){
+    //检查信息是否填写完整
     if(this.data.date==null||this.data.content==null){
-      console.log("信息还不完整哟！检查一下时间和内容吖");
-       
+      Notify('信息填写不完整，请检查时间和目标');
     }else{
-    // 怎么获取data中的check值？
-    if(this.data.check==true){
-      console.log("点击了上传");
-     this.setData({
-       modalHidden:!this.data.modalHidden
-     });
-    }
-    console.log(this.data.content);
-    if(this.data.modalHidden==true){
-      console.log("保存成共");
-   wx.showToast({
-     title: '保存成功',
-     icon:'success',
-     duration:1500,
-     mask:true
-   })
-    }
+      // let id = e.currentTarget.dataset.id;
+      for(let i=0;i<this.data.buttons.length;i++){
+        if(this.data.buttons[i].id==id){
+         this.data.targetLabel=this.data.buttons[i].name
+        }
+      }
+      console.log("标签："+this.data.targetLabel);
+       //将数据写入数据库中
+      db.collection('target').add({
+        data: {
+          endtime: this.data.date,
+          targetDetail: this.data.content,
+          targetLabel: this.data.targetLabel,
+          isUpload: this.data.isUpload,
+          isAnonymous: this.data.isAnonymous
+        }
+      }).then(res => {
+        console.log(res);
+        //成功返回成功提示
+        wx.showToast({
+          title: '保存成功',
+          icon: 'success',
+          duration: 1500,
+          mask: true
+        });
+        wx.switchTab({
+          url: '../dcIndex/dcIndex',
+        })
+        this.onLoad();
+      }).catch(err => {
+        console.log(err);
+      });
     }
   },
   // 确认匿名,需要给后台传匿名
   modalBindconfirm(){
     this.setData({
-      modalHidden:!this.data.modalHidden
+      modalHidden:!this.data.modalHidden,
+      isAnonymous:!this.data.isAnonymous
     });
-    // wx.showToast({
-    //   title: '匿名成功',
-    //   icon:'success',
-    //   duration:2500,
-    //   mask:true,
-    // });
     wx.showToast({
-      title: '保存成功',
+      title: '匿名成功',
       icon: 'success',
       duration: 1500,
       mask: true
@@ -128,54 +146,10 @@ Page({
       icon: 'success',
       duration: 1500,
       mask: true
-    })
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    });
+    this.setData({
+      modalHidden: !this.data.modalHidden,
+      isAnonymous:!this.data.isAnonymous
+    });
   }
 })
