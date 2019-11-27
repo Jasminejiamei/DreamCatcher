9bd54d867fd3420c8874e2bb96c1c30d12be1390
Emily9901 <mf1632563012@163.com>
2019-11-27 16:32:57 +0800

    目标广场

--- miniprogram/pages/targetProgress/targetProgress.js
+++ miniprogram/pages/targetProgress/targetProgress.js
@@ -6,13 +6,29 @@ Page({
    */
   data: {
 
+    
+    //进度列表
+    Progresslist: [{ avatar: "/images/targetground_ceshi.png", name: 'duck', date: '2019-11-10', text: '', color: "#FFCC66", tag: '运动', percent: "60%" }
+    ],
+    Progresslist_test1: [{ avatar: "/images/targetground_ceshi.png", name: 'duck', date: '2019-11-10', text: '', color: "#D4F9C8", tag: '技能', percent: "40%" }
+    ]
+
   },
 
+  
+  getProgresslist: function () {
+    this.setData({
+      Progresslist: this.data.Progresslist.concat(this.data.Pogresslist_test1)
+    });
+  },
+ 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.setData({
+      Progresslist: this.data.Progresslist.concat(this.data.Progresslist_test1)
+    });
   },
 
   /**
@@ -40,7 +56,9 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+    wx.redirectTo({
+      url: '../dcindex1/dcindex1'      //这里的跳转不是很流畅
+    })
   },
 
   /**
@@ -54,7 +72,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+   
   },
 
   /**
